f6685ad56f910964be6d29b60b1a504f4fcfc79c
tomporat247 <tomporat247@gmail.com>
2022-01-14 13:11:09 +0200

    add Dependencies.jsx

--- src/components/steps/Dependencies.jsx
+++ src/components/steps/Dependencies.jsx
@@ -1,6 +1,36 @@
+import { Button, ButtonGroup } from "@material-ui/core";
+import { useEffect, useState } from "react";
+
 const Dependencies = () => {
-  // TODO: The use callback case
-  return <div>xxx</div>;
+  const [counter, setCounter] = useState(0);
+
+  const options = ["update", "delete"];
+
+  const onOptionClick = (option) => {
+    const newCounter = counter + 1;
+    console.log(`clicked ${newCounter} times`);
+    setCounter(newCounter);
+  };
+
+  return <Options options={options} onOptionClick={onOptionClick} />;
+};
+
+const Options = ({ options, onOptionClick }) => {
+  useEffect(() => {
+    console.log("options changed");
+    console.log(`showing options: ${options.join(", ")}`);
+    // Notify the user a new option is available
+  }, [options]);
+
+  return (
+    <ButtonGroup color="primary" aria-label="outlined primary button group">
+      {options.map((option) => (
+        <Button key={option} onClick={() => onOptionClick(option)}>
+          {option}
+        </Button>
+      ))}
+    </ButtonGroup>
+  );
 };
 
 export default Dependencies;
