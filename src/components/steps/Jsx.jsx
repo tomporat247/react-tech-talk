558098ef529bf3024bd8ff0828aacce371a8aa19
tomporat247 <tomporat247@gmail.com>
2022-01-15 22:15:40 +0200

    different text

--- src/components/steps/Jsx.jsx
+++ src/components/steps/Jsx.jsx
@@ -3,13 +3,13 @@ const Jsx = () => {
     <main>
       <h1 id="title">Look at the console</h1>
       <p>paragraph</p>
-      <Inner>this text</Inner>
+      <Inner>Change my name</Inner>
     </main>
   );
 };
 
 const Inner = ({ children }) => {
-  return <div>hello from inner, got {children} from parent</div>;
+  return <div>hello from inner, got "{children}" from parent</div>;
 };
 
 console.log("This is what console.log(ThisComponent()) looks like:\n", Jsx());
